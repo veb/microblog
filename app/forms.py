df947981af3089640e1ac9fa6bc8a5300f209da1
Michael Mackenzie <vebbed@gmail.com>
2013-09-28 18:03:04 +1200

    adding in form edit validators

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,9 @@
-from flask.ext.wtf import Form, Textfield, BooleanField, TextAreaField
-from flask.ext.wtf import Required, Length
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import Form, TextField, BooleanField, TextField, BooleanField, TextAreaField, validators
 
 class EditForm(Form):
-    nickname = TextField('nickname', validators = [Required()])
-    about_me = TextAreaField('about_me', validators[Length(min = 0, max = 140)])
+    nickname = TextField('nickname', [validators.Required()])
+    about_me = TextAreaField('about_me', [validators.Length(min = 0, max = 140)])
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+    openid = TextField('openid', [validators.Required()])
     remember_me = BooleanField('remember_me', default = False)
