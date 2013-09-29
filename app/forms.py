fb03ffa718418394ae127cdf362f7cb2093e499e
Michael Mackenzie <vebbed@gmail.com>
2013-09-29 16:00:47 +1300

    needed custom validators

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,22 @@ class EditForm(Form):
     nickname = TextField('nickname', [validators.Required()])
     about_me = TextAreaField('about_me', [validators.Length(min = 0, max = 140)])
 
+    def __init__(self, original_nickname, *args, **kwargs):
+        Form.__init__(self, *args, $kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+
+        user = User.query.filter_by(nickname = self.nickname.data).first()
+        if user != None:
+            self.nickname.errors.append('This nickname is already in use'.)
+            return False
+        return True
+
 class LoginForm(Form):
     openid = TextField('openid', [validators.Required()])
     remember_me = BooleanField('remember_me', default = False)
