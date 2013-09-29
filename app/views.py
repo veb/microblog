a40478853dfaa986afe0981e2d912516066a61bf
Michael Mackenzie <vebbed@gmail.com>
2013-09-29 15:16:51 +1300

    added some environments and 404 page

--- app/views.py
+++ app/views.py
@@ -113,3 +113,12 @@ def edit():
 
     return render_template('edit.html',
             form = form)
+
+@app.errorhandler(404)
+def internal_error(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html')
--- run.py
+++
@@ -1,7 +0,0 @@
-#!flask/bin/python
-from app import app
-
-SERVER_NAME = '0.0.0.0'
-SERVER_PORT = 5000
-
-app.run(SERVER_NAME, SERVER_PORT, debug = True)
