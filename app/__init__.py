027f3253948fa207704439335fe4bb219cee5b32
Michael Mackenzie <vebbed@gmail.com>
2013-09-29 15:45:21 +1300

    adding logging to find bugs

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir
-
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -16,5 +15,14 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
+if not app.debug:
+	import logging
+	from logging.handlers import RotatingFileHandler
+	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024, 10 )
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	app.logger.setLevel(logging.INFO)
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.info('microblog startup')
 
 from app import views, models
--- config.py
+++ config.py
@@ -10,3 +10,9 @@ SECRET_KEY = 'HJSD68TU2368WEYUIYUSHEQGYUDSGHAGSDFYU3Q'
 OPENID_PROVIDERS = [
         { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' }]
 
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+ADMINS = ['mike@veb.co.nz']
