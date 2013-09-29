a1fe02ab03c963ef9bb7aad125551b724ef7f36e
Michael Mackenzie <vebbed@gmail.com>
2013-09-29 19:34:47 +1300

    fixed a bug that occured by poor eyesight

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 followers = db.Table('followers',
-        db.Column('follower_id', db.Integer, db.ForeignKey('user_id')),
-        db.Column('followed_id', db.Integer, db.ForeignKey('user_id'))
+        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 class User(db.Model):
