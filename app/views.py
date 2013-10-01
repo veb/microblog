d38f4015d96e0541fbb7ee9b6d99bfd1c7e00071
Michael Mackenzie <vebbed@gmail.com>
2013-10-01 22:20:06 +1300

    added follw/unfollow function

--- app/views.py
+++ app/views.py
@@ -63,9 +63,12 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-	nickname = User.make_unique_nickname(nickname)
+        nickname = User.make_unique_nickname(nickname)
         user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
         db.session.add(user)
+        db.session.commit()
+        # make suer follow themselves
+        db.session.add(user.follow(user))
         db.session.commit()
     remember_me = False
 
@@ -114,6 +117,42 @@ def edit():
     return render_template('edit.html',
             form = form)
 
+@app.route('/follow/<nickname>')
+def follow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t follow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + nickname + '.')
+        return redirect(url_for('user', nickname = nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+def unfollow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname = nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname = nickname))
+
 @app.errorhandler(404)
 def internal_error(error):
 	return render_template('404.html'), 404
@@ -121,4 +160,4 @@ def internal_error(error):
 @app.errorhandler(500)
 def internal_error(error):
 	db.session.rollback()
-	return render_template('500.html')
+    	return render_template('500.html')
